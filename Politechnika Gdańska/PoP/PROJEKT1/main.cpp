ae279345d3fb9712f036ed51e46cb7c9479fce6a
fist_it <53796140+fist-it@users.noreply.github.com>
2023-11-22 15:28:17 +0100

    Start of PoP project

--- Politechnika Gdańska/PoP/PROJEKT1/main.cpp
+++ Politechnika Gdańska/PoP/PROJEKT1/main.cpp
@@ -1,1 +1,15 @@
-#include<ncurses.h>
+#include <conio.h>
+using namespace std;
+/*
+TODO:
+-Choose the best data structures for the game
+-learn conio.h in practice
+*/
+
+
+
+
+
+int main(){
+    return 0;
+}
